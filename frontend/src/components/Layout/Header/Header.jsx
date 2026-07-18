1fb7f170bd3cdb57a7a2ebf1348951d369cf14aa
eduardocesl <eduardocesl@outlook.com>
2026-07-18 00:32:36 -0300

    Created FuelTrack navigation header

--- frontend/src/components/Layout/Header/Header.jsx
+++ frontend/src/components/Layout/Header/Header.jsx
@@ -1,9 +1,48 @@
 import "./Header.css";
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/logo.png";
 
 function Header() {
   return (
     <header>
-      <h2>FuelTrack</h2>
+      <div className="brand">
+        <img
+          src={logo}
+          alt="FuelTrack logo"
+        />
+
+        <h2>FuelTrack</h2>
+      </div>
+
+      <nav>
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? "active" : ""
+          }
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+          to="/vehicles"
+          className={({ isActive }) =>
+            isActive ? "active" : ""
+          }
+        >
+          Vehicles
+        </NavLink>
+
+        <NavLink
+          to="/fuelings"
+          className={({ isActive }) =>
+            isActive ? "active" : ""
+          }
+        >
+          Fuelings
+        </NavLink>
+      </nav>
     </header>
   );
 }
